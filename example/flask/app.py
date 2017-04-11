8500ab4f250ba4f228bdc4070e9f8700f95ac62c
Jakub Kulík <kulikjak@gmail.com>
2017-04-11 18:06:37 +0200

    Example changes and updates

--- example/flask/app.py
+++ example/flask/app.py
@@ -10,31 +10,30 @@ app.config.from_pyfile('config.py')
 logger = logging.getLogger(__name__)
 
 skautis = SkautisApi(app.config['SKAUTIS_APPID'], test=True)
+skautis_token = None
 
 
 @app.route('/hello')
 def authors():
-    return app.config['SKAUTIS_APPID'], 200
+    return 'Hello World!', 200
 
-
-@app.route('/index')
+@app.route('/')
 def index():
-    # Check if user is logged in
-    #if g.user:
-        # Render out page for authorized users.
-    #    return render_template('index.html', app_name="skautIS-py test", user=g.user)
-
-    # Render out the login form
+    # Render out the login page
     return render_template('login.html', 
                            login_link=skautis.get_login_url(),
                            app_name=app.config['APP_NAME'])
 
 @app.route('/login', methods=['POST'])
 def login():
+    global skautis_token
     skautis_token = request.form['skautIS_Token']
     skautis_idrole = request.form['skautIS_IDRole']
     skautis_idunit = request.form['skautIS_IDUnit']
     skautis_datelogout = request.form['skautIS_DateLogout']
+
+    user_info = skautis.UserManagement.UserDetail(skautis_token, None)
+    #role_info = skautis.UserManagement.RoleAll(skautis_token)
 
     return render_template('index.html',
                            login_link=skautis.get_login_url(),
@@ -42,9 +41,10 @@ def login():
                            skautis_token=skautis_token,
                            skautis_idunit=skautis_idunit,
                            skautis_idrole=skautis_idrole,
-                           skautis_datelogout=skautis_datelogout)
+                           skautis_datelogout=skautis_datelogout,
+                           user_info=user_info)
 
 @app.route('/logout')
 def logout():
-    # session.pop('user', None)
+    skautis.UserManagement.LoginUpdateLogout(skautis_token)
     return redirect(url_for('index'))
