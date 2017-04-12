c4c7bdf79510f944caba155009028f5a10d28461
Jakub Kulík <kulikjak@gmail.com>
2017-04-12 11:04:12 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='skautis-py',
+    version='1.0.0',
+    packages=["skautis"],
+    install_requires=[
+        'zeep',
+    ],
+)
