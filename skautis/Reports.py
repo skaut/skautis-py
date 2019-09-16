f619e35c7c61955ef89c0fe2f0cc5fadb7dca3cb
Jakub Kulík <kulikjak@gmail.com>
2019-09-16 12:00:13 +0200

    Auto commit - 16 Sep 2019

--- skautis/Reports.py
+++ skautis/Reports.py
@@ -101,8 +101,8 @@ class Reports(object):
         return self._client.service.EnrollInvoiceDetailBarcode({"ID_Login": ID_Login, "ID": ID, "SpaydString": SpaydString})
 
     # Tisková sestava: Detail 2F pro uživatele
-    def UserDetailTwoFactor(self, ID_Login):
-        return self._client.service.UserDetailTwoFactor({"ID_Login": ID_Login})
+    def UserDetailTwoFactor(self, ID_Login, ID):
+        return self._client.service.UserDetailTwoFactor({"ID_Login": ID_Login, "ID": ID})
 
     # Tisková sestava: Likvidační protokol
     def WarehouseItemStockTakingDiscarded(self, ID_Login, ID):
