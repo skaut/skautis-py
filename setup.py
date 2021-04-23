d4ed8f2df416bc2507ea364a0083a0aeadcaedc1
Jakub Kulík <kulikjak@gmail.com>
2021-04-23 12:00:10 +0200

    Auto commit - 23 Apr 2021

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skautis-py',
-    version='1.0.54',
+    version='1.0.55',
     description='Python library for interaction with the Skautis API',
     author='Jakub Kulik',
     author_email='kulikjak@gmail.com',
