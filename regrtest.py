162cff7bed7d2efd68e391d413622ef1a0a052ea
Automatic commit <kulikjak@users.noreply.github.com>
2021-10-28 21:04:41 +0200

    Add simple import test before making a new version

---
+++ regrtest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# For now, this is the best test I can think of. But considering
+# that most of the functionality is in the zeep library, this
+# should be sufficient.
+
+from skautis import SkautisApi
