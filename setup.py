b3b9225a6cfa7059910ba3a6b6be2dbbe81d619a
Automatic commit <kulikjak@users.noreply.github.com>
2023-01-04 00:18:16 +0000

    Auto commit version 1.1.8 - 04 Jan 2023

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skautis-py',
-    version='1.1.7',
+    version='1.1.8',
     description='Python library for interaction with the Skautis API',
     author='Jakub Kulik',
     author_email='kulikjak@gmail.com',
