3fe231f76932b8c3ab159c6a39cbdf8f853b19fd
Automatic commit <kulikjak@users.noreply.github.com>
2025-01-12 00:25:58 +0000

    Auto commit version 1.1.32 - 12 Jan 2025

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='skautis-py',
-    version='1.1.31',
+    version='1.1.32',
     description='Python library for interaction with the Skautis API',
     author='Jakub Kulik',
     author_email='kulikjak@gmail.com',
